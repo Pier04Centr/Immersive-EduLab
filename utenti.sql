90bdb5d210632f833b79f4dd1f1d11ae8d176aea
Pierpaolo Centrone <p.centrone3@studenti.poliba.it>
2025-12-07 16:47:39 +0100

    aggiunto db utenti in commit

---
+++ utenti.sql
@@ -0,0 +1,45 @@
+-- 1. Creazione e selezione del Database (Perfetto, lasciamo così)
+CREATE DATABASE IF NOT EXISTS utenti;
+USE utenti;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Struttura della tabella `utenti`
+--
+
+CREATE TABLE `utenti` (
+  `Username` varchar(50) NOT NULL,
+  `Nome` varchar(50) NOT NULL,
+  `Cognome` varchar(50) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+  -- RIMOSSA LA VIRGOLA QUI SOPRA
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indici per le tabelle `utenti`
+--
+ALTER TABLE `utenti`
+  ADD PRIMARY KEY (`Username`); 
+  -- RIMOSSA LA VIRGOLA QUI SOPRA E MESSO PUNTO E VIRGOLA
+
+-- Svuota la tabella se c'erano dati vecchi
+TRUNCATE TABLE `utenti`;
+
+INSERT INTO `utenti` (`Username`, `Nome`, `Cognome`, `Email`, `Password`) VALUES
+('test', 'Mario', 'Rossi','test@mail.com', sha1('P@ssw0rd1'));
+
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
