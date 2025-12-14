e7a5a890dd07438fac84676d74109bc205062d96
Pierpaolo Centrone <p.centrone3@studenti.poliba.it>
2025-12-14 20:23:33 +0100

    alliemanto colori e font con homepage

--- index.php
+++ index.php
@@ -42,12 +42,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Immersive EduLab</title>
+    <title>EduLab</title>
     
     <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.3.0/model-viewer.min.js"></script>
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
-
+    
+    <link rel="stylesheet" href="style.css?v=2">
 </head>
 <body>
 
